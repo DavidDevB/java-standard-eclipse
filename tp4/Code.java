e52e8886d300ac24596c1d4757afa88dece7ed46
DavidB <david.bernard@fms-ea.com>
2025-11-04 13:25:57 +0100

    verification with hasnetint et scan.next added + verification if exactly 4 numbers are entered

--- tp4/Code.java
+++ tp4/Code.java
@@ -1,6 +1,8 @@
 package tp4;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -28,10 +30,26 @@ public class Code {
 		
 		// Boucle qui gère l'entrée de la réponse de l'utilisateur.
 		while (!enter2) {
-			System.out.println("Enter a four digits code.");
-			guess = scan.nextInt();
+			System.out.println("Enter a four digits code:");
+			
+			
+			// Vérifie que l'entrée est bien un entier.
+			if (scan.hasNextInt()) {
+	            guess = scan.nextInt();
+	        } else {
+	            System.out.println("It's not an integer !");
+	            scan.next();
+	        }
+			
+			// Vérifie que le nombre est bien composé de 4 et 4 chiffres seulement.
 			if (String.valueOf(guess).length() != 4) {
-				System.out.println("Please enter a four digits code.");
+				System.out.println("Enter four digits please.");
+				continue;
+			}
+			
+			//Vérifie si le guess a que des chiffres différents.
+			if (!hasUniqueDigits(guess)) {
+				System.out.println("Enter only unique digits.");
 				continue;
 			}
 			
@@ -40,7 +58,7 @@ public class Code {
 			String[] splittedGuess = String.valueOf(guess).split("");
 			
 			
-			// Parcours le tablaeu codeToGuess et vérifie si les elements a la meme positions sont égaux ou si il contient l'élement en question.
+			// Parcours le tableau codeToGuess et vérifie si les elements a la même positions sont égaux ou si il contient l'élement en question.
 			// Incrémente les counters correspondants.
 			for (int i = 0; i < codeToGuess.length; i++) {
 				if (splittedGuess[i].equals(codeToGuess[i])) {
@@ -70,6 +88,30 @@ public class Code {
 		
 	}
 	
+	
+	public static boolean hasUniqueDigits(int number) {
+
+        boolean[] digitSeen = new boolean[10];
+        while (number > 0) {
+            int digit = number % 10; // utilise le dernier digit du nombre.
+            if (digitSeen[digit]) {
+                return false; // Si c'est déjà true dans le tableau de booléens retourne false.
+            }
+            digitSeen[digit] = true; // Retourne true si pas d'occurrence déjà trouvée.
+            number /= 10; // Supprime le dernier digit et passe au nombre restant.
+        }
+        return true; 
+    }
+	
+	public static boolean containsNumbersPatternMatcher(String input) {
+        /**
+         * Checks if an input matches the regex pattern. Returns a boolean.
+         */
+        Pattern p = Pattern.compile("^-?\\d+$");
+        Matcher m = p.matcher(input);
+        return m.matches();
+    }
+    
 	
 	
 	
